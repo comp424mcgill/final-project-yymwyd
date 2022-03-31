687305c1698fd3fdacbc96f27864dfe621480fb6
Celine Wu <wuyudan2001@icloud.com>
2022-03-31 15:57:51 -0400

    setter and getter for nodes

--- MCT.py
+++ MCT.py
@@ -4,10 +4,10 @@ class Node:
         #initiate MCT 
         def __init__(self, num_visited, num_success):
                 #initialize node number of visited
-                self.num_visited = 0;
+                self.num_visited = 0
                 #initialize node number of success
-                self.num_success = 0;
-                self.children = dict();
+                self.num_success = 0
+                self.children = dict()
                 
                 
                 
--- Node.py
+++ Node.py
@@ -9,6 +9,20 @@ class Node:
                 self.num_success = 0
                 self.my_pos = my_pos
                 self.dir = dir
+
+        #getter for  nodes
+        def get_visits(self):
+                return self.num_visited
+
+        def get_wins(self):
+                return self.num_success
+
+        #setter for nodes
+        def set_visits(self, v):
+                self.num_visited = v
+
+        def set_wins(self,w):
+                self.num_success=w
                 
                 
                 
