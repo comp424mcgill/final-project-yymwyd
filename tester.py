a6dd9a644edf6916445b3d62e9c7fe5f471b75a3
Yimei-Yang <101018498+Yimei-Yang@users.noreply.github.com>
2022-04-02 21:00:07 -0400

    desperate update: how to find all legal actions????

--- tester.py
+++ tester.py
@@ -4,54 +4,64 @@ from node import Node
 
 #unit test for backpropagation
 #assume all those are successful
-def backpropagation(listOfPaths):
-    pass
+def updateNode(n, success):
+    newVisit = n.get_visits() + 1
+    n.set_visits(newVisit)
+    if(success):
+        newWin = n.get_wins() + 1
+        n.set_wins(newWin)
+
 def main():
     listOfPaths = []
-    Path1 = []
-    Path2 = []
-    Path3 = []
+    updatedList = []
+    path1 = []
+    path2 = []
+    path3 = []
     
-    N1 = Node((1,2),0)
-    N2 = Node((3,4),1)
-    N3 = Node((5,6),2)
-    Path1.append(N1)
-    Path1.append(N2)
-    Path1.append(N3)
+    n1 = Node((1,2),0)
+    n2 = Node((3,4),1)
+    n3 = Node((5,6),2)
+    path1.append(n1)
+    path1.append(n2)
+    path1.append(n3)
 
-    N4 = Node((0,1),3)
-    N5 = Node((2,2),1)
-    N6 = Node((7,0),2)
-    Path2.append(N4)
-    Path2.append(N5)
-    Path2.append(N6)
+    n4 = Node((0,1),3)
+    n5 = Node((2,2),1)
+    n6 = Node((7,0),2)
+    path2.append(n4)
+    path2.append(n5)
+    path2.append(n6)
 
-    N7 = Node((6,5),0)
-    N8 = Node((1,1),1)
-    N9 = Node((3,2),2)
-    Path3.append(N7)
-    Path3.append(N8)
-    Path3.append(N9)
-
-    listOfPaths.append(Path1)
-    listOfPaths.append(Path2)
-    listOfPaths.append(Path3)
-
-    updatedList = backpropagation(listOfPaths)
-
+    n7 = Node((6,5),0)
+    n8 = Node((1,1),1)
+    n9 = Node((3,2),2)
+    path3.append(n7)
+    path3.append(n8)
+    path3.append(n9)
+    listOfPaths.append(path1)
+    listOfPaths.append(path2)
+    listOfPaths.append(path3)
     #show values before backpropagation
     for list in listOfPaths:
         for n in list:
-            print("This is the updated number of success:\n", n.get_wins())
-            print("This is the updated number of visited:\n", n.get_visits())
+            print("This is the original number of success:\n", n.get_wins())
+            print("This is the original number of visited:\n", n.get_visits())
 
-    '''
-    NumPath = len(updatedList)
+
+    updatedPath1 = backpropagation(path1, True)
+    updatedPath2 = backpropagation(path2, False)
+    updatedPath3 = backpropagation(path3, False)
+
+    updatedList.append(updatedPath1)
+    updatedList.append(updatedPath2)
+    updatedList.append(updatedPath3)
+
     #show values are updated
     for list in updatedList:
+        print("one path\n")
         for n in list:
             print("This is the updated number of success:\n", n.get_wins())
             print("This is the updated number of visited:\n", n.get_visits())
-'''
+
 if __name__ == "__main__":
     main()
